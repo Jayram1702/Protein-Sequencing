a528eac1fdf46ec22a6abfc94f1802494fcc922a
Jayaram1702 <ram999jayaram@msitprogram.net>
2022-01-25 10:51:50 +0530

    done code for get DNA data

--- hw6_protein.py
+++ hw6_protein.py
@@ -17,8 +17,11 @@ Parameters: str
 Returns: str
 '''
 def readFile(filename):
-    return
-
+    file = open(filename,"r").read()
+    stri = ""
+    for line in file.splitlines():
+        stri = stri+line
+    return stri
 
 '''
 dnaToRna(dna, startIndex)
@@ -190,6 +193,7 @@ if __name__ == "__main__":
     test.week1Tests()
     print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     runWeek1()
+    test.testReadFile()
 
     ## Uncomment these for Week 2 ##
     """
