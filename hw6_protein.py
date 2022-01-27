8006ae7face788f3aac8627116b61e3a593f7508
Jayaram1702 <ram999jayaram@msitprogram.net>
2022-01-27 19:11:05 +0530

    done code for Create Bar Chart

--- hw6_protein.py
+++ hw6_protein.py
@@ -278,6 +278,15 @@ Returns: None
 '''
 def createChart(xLabels, freqList1, label1, freqList2, label2, edgeList=None):
     import matplotlib.pyplot as plt
+    w = 0.35
+    plt.bar(xLabels, freqList1, width=-w, align='edge', label=label1)
+    plt.bar(xLabels, freqList2, width= w, align='edge', label=label2)
+
+    plt.xticks(rotation="vertical")
+    plt.legend()
+    plt.title("Compare Human and Elephant Genes")
+
+    plt.show()
     return
 
 
@@ -336,4 +345,5 @@ if __name__ == "__main__":
     runFullProgram()
     """
     # test. testMakeAminoAcidLabels()
-    test.testSetupChartData()
+    # test.testSetupChartData()
+    test.testCreateChart()
